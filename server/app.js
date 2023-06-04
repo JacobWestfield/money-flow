f10b39f6b3c2f321a2bb807e5c91650c36faa8f0
JacobWestfield <zzultekk@gmail.com>
2023-06-04 22:53:40 +0300

    patch deploy stage

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 const chalk = require("chalk");
 const cors = require("cors");
+const path = require("path");
 const routes = require("./routes");
 
 const app = express();
@@ -15,17 +16,18 @@ app.use("/api", routes);
 
 const PORT = config.get("port") ?? 8080;
 
-// if (process.env.NODE_ENV === "production") {
-//   console.log("Production");
-// } else {
-//   console.log("Development");
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client")));
+
+  const indexPath = path.join(__dirname, "client", "index.html");
+
+  app.get("*", (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
 
 async function start() {
   try {
-    // mongoose.connection.once("open", () => {
-    //   initDatabase();
-    // });
     await mongoose.connect(config.get("mongoUri"));
     console.log(chalk.bgCyan.bold("Connected to DataBase"));
     app.listen(PORT, () => {
